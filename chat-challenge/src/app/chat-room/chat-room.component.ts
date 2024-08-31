92abf630458d540e5d03812318d8ea9570a9c7d2
114230-RAPACCIONI <128556512+114230-RAPACCIONI@users.noreply.github.com>
2024-08-31 15:50:47 -0300

    ultimo commit

--- chat-challenge/src/app/chat-room/chat-room.component.ts
+++ chat-challenge/src/app/chat-room/chat-room.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MessageUserComponent } from '../message-user/message-user.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chat-room',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MessageUserComponent],
   templateUrl: './chat-room.component.html',
   styleUrl: './chat-room.component.css'
 })
 export class ChatRoomComponent {
+  user1 = 'Pepe';
+  user2 = 'Maria';
 
+  messages: {user: string, text: string}[] = [];
+
+  addMessage(message: {user: string, text: string}){
+    this.messages.push(message);
+  }
 }
--- chat-challenge/src/app/message-user/message-user.component.ts
+++ chat-challenge/src/app/message-user/message-user.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-message-user',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './message-user.component.html',
   styleUrl: './message-user.component.css'
 })
 export class MessageUserComponent {
+  @Input() user: string = '';
+  @Output() messageSent = new EventEmitter<{user: string, text: string}>();
+  message: string = '';
 
+
+  sendMessage() {
+    if(this.message.trim()){
+      this.messageSent.emit({user: this.user, text: this.message})
+      this.message = '';
+    }
+  }
+  
 }
